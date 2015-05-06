ec837c458c67ce46515d1ca4b951515b29394469
Brian Grey <mixolyde@gmail.com>
2015-05-06 09:09:44 -0400

    Extra card orientation tests

--- dartbase_server/test/gamelogic/card_test.dart
+++ dartbase_server/test/gamelogic/card_test.dart
@@ -9,6 +9,11 @@ void main() {
   group('card tests', () {
     test('card orientation', () {
       expect(CardOrientation.UP, CardOrientationUtil.opposite(CardOrientation.DOWN));
+      expect(CardOrientation.RIGHT, CardOrientationUtil.opposite(CardOrientation.LEFT));
+      expect(CardOrientation.RIGHT, CardOrientationUtil.cw(CardOrientation.UP));
+      expect(CardOrientation.RIGHT, CardOrientationUtil.ccw(CardOrientation.DOWN));
+      expect(CardOrientation.UP, CardOrientationUtil.cw(CardOrientation.LEFT));
+      expect(CardOrientation.UP, CardOrientationUtil.ccw(CardOrientation.RIGHT));
       
     });
   });
