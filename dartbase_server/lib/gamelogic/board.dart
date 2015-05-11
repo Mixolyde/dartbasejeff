64b16eecff999bab0f42089b621c1cd875c3d2ad
Brian Grey <mixolyde@gmail.com>
2015-05-10 22:55:57 -0400

    Started board classes

--- dartbase_server/lib/gamelogic/board.dart
+++ dartbase_server/lib/gamelogic/board.dart
@@ -1,1 +1,22 @@
 part of dartbase_server;
+
+class BoardLoc{
+  final int x;
+  final int y;
+  const BoardLoc(this.x, this.y);
+
+  String toString() => "{$x, $y}";
+
+  BoardLoc neighborLoc(CardOrientation dir) {
+    switch (dir) {
+      case CardOrientation.up:
+        return new BoardLoc(this.x, this.y + 1);
+      case CardOrientation.down:
+        return new BoardLoc(this.x, this.y - 1);
+      case CardOrientation.left:
+        return new BoardLoc(this.x - 1, this.y);
+      case CardOrientation.right:
+        return new BoardLoc(this.x + 1, this.y);
+    }
+  }
+}
--- dartbase_server/test/gamelogic/board_test.dart
+++ dartbase_server/test/gamelogic/board_test.dart
@@ -5,14 +5,10 @@ import 'package:unittest/unittest.dart';
 import 'package:dartbase_server/dartbase_server.dart';
 
 void main() {
-  group('card tests', () {
-    test('card orientation', () {
-      expect(CardOrientation.up, CardUtil.opposite(CardOrientation.down));
-      expect(CardOrientation.right, CardUtil.opposite(CardOrientation.left));
-      expect(CardOrientation.right, CardUtil.cw(CardOrientation.up));
-      expect(CardOrientation.right, CardUtil.ccw(CardOrientation.down));
-      expect(CardOrientation.up, CardUtil.cw(CardOrientation.left));
-      expect(CardOrientation.up, CardUtil.ccw(CardOrientation.right));
+  group('board location tests', () {
+    test('neighbor location', () {
+      var origin = const BoardLoc(0, 0);
+      expect(const BoardLoc(1,0), origin.neighborLoc(CardOrientation.right));
     });
   });
 }
