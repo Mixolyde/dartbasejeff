9734aa6a41d7e6866e31dfb92f793471210cbd77
Brian Grey <mixolyde@gmail.com>
2015-05-13 14:09:10 -0400

    Reorganized card util tests

--- dartbase_server/test/gamelogic/card_test.dart
+++ dartbase_server/test/gamelogic/card_test.dart
@@ -5,48 +5,58 @@ import 'package:unittest/unittest.dart';
 import 'package:dartbase_server/dartbase_server.dart';
 
 void main() {
-  group('card tests', () {
-    test('card orientation util', () {
+  group('card util tests', () {
+    test('card util opposite orientation', () {
       expect(CardOrientation.up, CardUtil.opposite(CardOrientation.down));
       expect(CardOrientation.right, CardUtil.opposite(CardOrientation.left));
+    });
+    test('card util clockwise orientation', () {
       expect(CardOrientation.right, CardUtil.cw(CardOrientation.up));
       expect(CardOrientation.right, CardUtil.ccw(CardOrientation.down));
+    });
+    test('card util counter-clockwise orientation', () {
       expect(CardOrientation.up, CardUtil.cw(CardOrientation.left));
       expect(CardOrientation.up, CardUtil.ccw(CardOrientation.right));
     });
 
-    test('card exits', () {
+    test('COM card exits', () {
       expect(1, CardUtil.exits(CardType.COM, CardOrientation.down).length);
       expect(CardUtil.exits(CardType.COM, CardOrientation.down), contains(CardOrientation.down));
-
+    });
+    test('LAB card exits', () {
       expect(2, CardUtil.exits(CardType.LAB, CardOrientation.down).length);
       expect(CardUtil.exits(CardType.LAB, CardOrientation.down), contains(CardOrientation.down));
       expect(CardUtil.exits(CardType.LAB, CardOrientation.down), contains(CardOrientation.left));
-
+    });
+    test('FAC card exits', () {
       expect(2, CardUtil.exits(CardType.FAC, CardOrientation.down).length);
       expect(CardUtil.exits(CardType.FAC, CardOrientation.down), contains(CardOrientation.down));
       expect(CardUtil.exits(CardType.FAC, CardOrientation.down), contains(CardOrientation.up));
-
+    });
+    test('HAB card exits', () {
       expect(3, CardUtil.exits(CardType.HAB, CardOrientation.down).length);
       expect(CardUtil.exits(CardType.HAB, CardOrientation.down), isNot(contains(CardOrientation.down)));
-
+    });
+    test('POW card exits', () {
       expect(4, CardUtil.exits(CardType.POW, CardOrientation.down).length);
       expect(CardUtil.exits(CardType.POW, CardOrientation.down), contains(CardOrientation.down));
-
+    });
+    test('SAB card exits', () {
       //TODO fix test to properly capture thrown error
       //expect(CardUtil.exits(CardType.SAB, CardOrientation.DOWN), throwsArgumentError);
+      expect(true, isTrue);
     });
   });
 
   group('deck tests', () {
-    test('new deck', () {
+    test('new deck card counts', () {
       expect(20, DeckUtil.shuffledDeck().length);
       expect(3, DeckUtil.shuffledDeck().where((card) => card.type == CardType.REC).length);
       expect(4, DeckUtil.shuffledDeck().where((card) => card.type == CardType.LAB).length);
       expect(2, DeckUtil.shuffledDeck().where((card) => card.type == CardType.SAB).length);
     });
 
-    test('same deck', () {
+    test('two shuffled decks are not the same deck', () {
       List<Card> testDeck = DeckUtil.shuffledDeck();
       List<Card> testDeck2 = DeckUtil.shuffledDeck();
 
