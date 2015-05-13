439ebcabd74693cb007a5e62cbd1b5a87d85c4a6
Brian Grey <mixolyde@gmail.com>
2015-05-13 13:48:04 -0400

    Added played card tests

--- dartbase_server/test/gamelogic/board_test.dart
+++ dartbase_server/test/gamelogic/board_test.dart
@@ -8,7 +8,25 @@ void main() {
   group('board location tests', () {
     test('neighbor location', () {
       var origin = const BoardLoc(0, 0);
-      expect(const BoardLoc(1,0), origin.neighborLoc(CardOrientation.right));
+      expect(origin.neighborLoc(CardOrientation.right), const BoardLoc(1,0));
+      expect(origin.neighborLoc(CardOrientation.left), const BoardLoc(-1,0));
+    });
+  });
+  
+  group('playerd card tests', () {
+    test('COM played card exits', () {
+      var pc = const PlayedCard(Card.COM, CardOrientation.down, 1);
+      expect(pc.exits().length, 1);
+      expect(pc.exits(), contains(CardOrientation.down));
+    });
+    
+    test('POW played card exits', () {
+      var pc = const PlayedCard(Card.POW, CardOrientation.down, 1);
+      expect(pc.exits().length, 4);
+      expect(pc.exits(), contains(CardOrientation.down));
+      expect(pc.exits(), contains(CardOrientation.up));
+      expect(pc.exits(), contains(CardOrientation.left));
+      expect(pc.exits(), contains(CardOrientation.right));
     });
   });
 }
