177a2ac36b4b249e222fc03b9ac03846ee60b634
Brian Grey <mixolyde@gmail.com>
2015-06-11 08:34:32 -0400

    Rearranged tests

--- dartbase_server/test/gamelogic/game_test.dart
+++ dartbase_server/test/gamelogic/game_test.dart
@@ -50,21 +50,7 @@ void main() {
       expect(game.isStarted, isTrue);
     });
   });
-  group('round tests', () {
-    test('round data init', () {
-      Player p1 = new Player(1, "Brian");
-      Player p2 = new Player(2, "Brian2");
-
-      Round round = new Round([p1, p2]);
-
-      expect(round.pot, 0);
-      expect(round.turnCount, 1);
-      expect(round.roundData.keys.length, 2);
-      expect(round.board.count, 0);
-      expect(round.selections.keys.length, 0);
-      expect(round.activePlayer, null);
-
-    });
+  group('make card selections', () {
     test('make some selections', () {
       //pick first card from 3/4 player's hands
       Game game = createSeededGame(4);
@@ -173,13 +159,27 @@ void main() {
 
   });
   group('player round data tests', () {
-    test('player round data init', () {
+    test('first player round data init', () {
       Player p1 = new Player(1, "Brian");
       PlayerRoundData data = new PlayerRoundData(p1);
 
       expect(data.hand.length, 5);
       expect(data.deferred.length, 0);
       expect(data.deck.length, 15);
+    });
+    test('two player round data init', () {
+      Player p1 = new Player(1, "Brian");
+      Player p2 = new Player(2, "Brian2");
+
+      Round round = new Round([p1, p2]);
+
+      expect(round.pot, 0);
+      expect(round.turnCount, 1);
+      expect(round.roundData.keys.length, 2);
+      expect(round.board.count, 0);
+      expect(round.selections.keys.length, 0);
+      expect(round.activePlayer, null);
+
     });
   });
 }
