2197e1c913e5f883fa4f7df27a0ffb2fc9fc4cda
Brian Grey <mixolyde@gmail.com>
2015-06-17 15:23:51 -0400

    Added card count map to deck utils

--- dartbase_server/lib/gamelogic/card.dart
+++ dartbase_server/lib/gamelogic/card.dart
@@ -100,6 +100,17 @@ class CardUtil {
 }
 
 class DeckUtil {
+  static const Map<Card, int> cardCounts = const {
+    Card.rec:3,
+    Card.doc:2,
+    Card.com:3,
+    Card.lab:4,
+    Card.fac:3,
+    Card.hab:2,
+    Card.pow:1,
+    Card.sab:2
+  };
+  
   static List<Card> _sortedDeck() {
     List<Card> cards = [];
     cards.addAll(new List.filled(3, Card.rec));
