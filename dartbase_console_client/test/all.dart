8ba0b260bf80ed430f1b68115f76f73465def733
Brian Grey <mixolyde@gmail.com>
2015-07-30 14:49:20 -0400

    Fix forgotten import statement

--- dartbase_console_client/test/all.dart
+++ dartbase_console_client/test/all.dart
@@ -1,10 +1,10 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2015, Brian G. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 library all_tests;
 
 import 'dartbase_console_client_test.dart' as dartbase_console_client_test;
-
+import 'print_test.dart' as print_test;
 
 void main() {
   dartbase_console_client_test.main();
