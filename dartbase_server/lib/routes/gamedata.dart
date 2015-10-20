587ed7dc46229ea6e3a72e78a75c8ceeb669099a
Brian Grey <mixolyde@gmail.com>
2015-10-20 19:23:20 -0400

    First game data fetching route

---
+++ dartbase_server/lib/routes/gamedata.dart
@@ -0,0 +1,24 @@
+// Copyright (c) 2015, Brian G. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The dartbase_server library.
+part of dartbase_server;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:isolate';
+import 'dart:math';
+import 'package:redstone/server.dart' as app;
+import './gamelogic.dart';
+
+@app.Route("/game/:gameid/playerdata/:playerid")
+Map getPlayerDataForGame(int gameid, int playerid) {
+  Map gameDataMap = {};
+  try {
+    gameDataMap['gameid'] = gameid;
+    gameDataMap['playerid'] = playerid;
+  } catch (e) {
+    log("Error getting PlayerData For Game " + gameid + ": $e");
+  }
+  return gameDataMap;
+}
