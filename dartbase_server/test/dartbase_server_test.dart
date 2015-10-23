8efc12a2416fed81c09a429dc9a49b48fe123be8
Brian Grey <mixolyde@gmail.com>
2015-10-23 10:05:16 -0400

    Rename redstone test methods

--- dartbase_server/test/dartbase_server_test.dart
+++ dartbase_server/test/dartbase_server_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2015, Brian G. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 library dartbase_server_test;
@@ -11,10 +11,10 @@ import 'package:dartbase_server/dartbase_server.dart';
 
 void main() {
   //load handlers in server library
-  setUp(() => web.setUp([#dartbase_server]));
+  setUp(() => web.redstoneSetUp([#dartbase_server]));
 
   //remove all loaded handlers
-  tearDown(() => web.tearDown());
+  tearDown(() => web.redstoneTearDown());
 
   serverTests();
 }
